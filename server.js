50b8d5ff8292228acbaacbd9e4c234735545b8e5
Dina Khouri <dina.khouri.91@gmail.com>
2019-03-17 12:23:06 -0500

    app sucessfully opens html pages on localhost, linked html pages, need to link api

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // require("./app/routing/apiRoutes")(app);
-// require("./app/routing/htmlRoutes")(app);
-app.get("/", (req, res) => res.send("Hello World!"));
+require("./app/routing/htmlRoutes")(app);
+// app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
