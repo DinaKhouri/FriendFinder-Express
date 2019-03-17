d1060bc86403e92ba2d2237cfcb32fa5ca7fde3f
Dina Khouri <dina.khouri.91@gmail.com>
2019-03-17 14:38:50 -0500

    started with the friends finder logic,it now grabs the input current user and shows it score summation

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,6 +4,30 @@ var friends = [
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Dina",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 3, 4, 5, 5, 5, 5, 4, 2]
+  },
+  {
+    name: "Niko",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 1, 4, 5, 1, 5, 5, 4, 2]
+  },
+  {
+    name: "Peter",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 5, 4, 5, 1, 2, 3, 4, 1]
+  },
+  {
+    name: "Nick",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 1, 5, 1, 2, 5, 5, 1]
   }
 ];
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,41 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+    // console.log(friends);
     //need to add friendfinder logic here
+    //req here is our user that is being posted
+    //req.body is this user's info
+    //req.body.scores is the scores array
+    //so to be able to make a comparison first we save our user info in a variable for ease of use
+    var OurUser = req.body;
+    var OurUserName = OurUser.name;
+    var OurUserScore = OurUser.scores;
+
+    //change list of integer to list of number
+    var OurUserScoreNumber = OurUserScore.map(Number);
+    function add(accumulator, a) {
+      return accumulator + a;
+    }
+    const sum = OurUserScoreNumber.reduce(add);
+    //function to sum the array
+
+    console.log(OurUserScoreNumber);
+    console.log(sum); // 6
+    // for (i = 0; i < 10; i++) {
+    //   OurUserScoreSum = 5;
+    //   return OurUserScoreSum;
+    // }
+    //console.log(OurUserScoreSum);
+    //now we need to run through the existing array and check the scores summation then compare it to OurUserScore
+    //but first let's create an array to hold each users score difference
+    // var SumArray = [];
+    // for (var i = 0; i < friends.length; i++) {
+    //   for (var y = 0; y < friends[i].scores.length; y++) {
+    //     var sum = +friends[i].scores[y];
+    //     SumArray.push(sum);
+    //   }
+    //   console.log(SumArray);
+    //}
     friends.push(req.body);
     res.json(true);
   });
