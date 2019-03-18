fa2c46f331b608af33617d980c9bd482215bcda5
Dina Khouri <dina.khouri.91@gmail.com>
2019-03-17 20:56:39 -0500

    added correct link to images, app fully working

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,31 +2,31 @@ var friends = [
   {
     name: "Ahmed",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "https://img2.thejournal.ie/inline/2470754/original/?width=428&version=2470754",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
     name: "Dina",
     photo:
-      "https://www.eharmony.co.uk/dating-advice/wp-content/uploads/2011/04/profilephotos.jpg",
+      "https://makehimyours.com.au/wp-content/uploads/2016/11/Depositphotos_9830876_l-2015Optimised.jpg",
     scores: [5, 1, 3, 4, 5, 5, 5, 5, 4, 2]
   },
   {
     name: "Niko",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "https://images.askmen.com/1080x540/2016/01/25-021526-facebook_profile_picture_affects_chances_of_getting_hired.jpg",
     scores: [5, 1, 1, 4, 5, 1, 5, 5, 4, 2]
   },
   {
     name: "Peter",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "http://ap.church/wp-content/uploads/2018/08/Brian-Jones-Profile.jpg",
     scores: [5, 1, 5, 4, 5, 1, 2, 3, 4, 1]
   },
   {
     name: "Nick",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "https://www.elitesingles.ie/sites/www.elitesingles.ie/files/styles/elite_rectangle_article_arrow_left_frame/public/2b_en_ta_teaser_sp1.png",
     scores: [5, 1, 4, 1, 5, 1, 2, 5, 5, 1]
   }
 ];
